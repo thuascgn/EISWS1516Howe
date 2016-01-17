03252fa91ac453b26367f46131652fc1a6f894f8
Tim <tim.howe@smail.fh-koeln.de>
2016-01-17 12:45:09 +0100

    /f

--- MS5/Prototyp_source/EISWS1516Howe/RuleService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RuleService.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- prototyp/EISWS1516Howe/RuleService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RuleService.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
